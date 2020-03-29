e0985f7a86d43698fd6fc769cb9b43afd01548a1
emil <emilbabazade29@gmail.com>
2020-03-29 22:13:59 +0300

    put the handlers for adding new person inside the file for AddNew component

--- phonebook/src/components/app.js
+++ phonebook/src/components/app.js
@@ -16,79 +16,16 @@ const App = () => {
   // TODO: new state for phone number input - done
   const [ newPhoneNumber, setNewPhoneNumber ] = useState(0)
 
-  // helper function, i use variable function definitions for components
-  // and normal function definition for anything else
-  // it just seperates them nicely 
-  function validatePerson( newPerson ) {
-    // check if empty
-    if( newPerson.name && !isNaN( !newPerson.number ) ){
-      // check if name or number already exists
-      var nameIsInPersons = persons.some( person => person.name === newPerson.name );
-      var phonenNumberIsInPersons = persons.some( person => person.number === newPerson.number );
-      if( nameIsInPersons ){
-        throw {
-          phoneBookException: true,
-          userText: `${ newPerson.name } is already in phone book`
-        }
-      } else if( phonenNumberIsInPersons ) {
-        var originalOwner = persons.reduce( ( name, person ) => {
-          if( person.number === newPerson.number ) {
-            return person.name
-          } 
-          return name
-        }, '' )
-        throw {
-          phoneBookException: true,
-          userText: `Number ${ newPerson.number } belongs to ${ originalOwner }`
-        }
-      }
-      // update the states
-    } else {
-      throw {
-        phoneBookException: true,
-        userText: 'None of the fields can be empty!' 
-      } 
-    }
-    
-  }
-
-  // TODO: add handler for submit button that adds a new person to persons - done
-  // TODO: change so phone numbers can be added - done
-  const addPerson = ( event ) => {
-    //not submitting anywhere
-    event.preventDefault()
-    let newPerson = { name: newName, number: newPhoneNumber }
-    try {
-      validatePerson( newPerson )
-      setPersons( persons.concat( newPerson ) )
-      setNewName( '' )
-      setNewPhoneNumber( 0 )
-    } catch (error) {
-      if( error.phoneBookException ) { // is a custom exception made in this project
-        alert( error.userText )
-      } else { // is an internal exception
-        throw error
-      }
-    }
-  } 
-
-  // TODO: handler for phone number input onChange
-  //that changes the value of the input box and newNumber - done
-  const handlePhoneNumberChange = ( event ) => setNewPhoneNumber( event.target.value )
-
-  // TODO: add handler for change on the input box 
-  // that changes the value of the input box and newName - done
-  const handleNameChange = ( event ) => setNewName( event.target.value )
-
   return (
     <div>
       <h1>Phonebook</h1>
       <AddNew 
         name={ newName }
-        nameChangeHandler={ handleNameChange }
+        nameSetter={ setNewName }
+        phoneNumSetter={ setNewPhoneNumber }
         phoneNumber={ newPhoneNumber }
-        phoneNumberChangeHandler={ handlePhoneNumberChange }
-        submitHandler={ addPerson }
+        persons={ persons }
+        personsSetter={ setPersons }
       />
       <ListAll persons={ persons } />
     </div>
